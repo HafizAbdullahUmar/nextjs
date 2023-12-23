8e74cce7b864715160a229381dca6dcb894c5206
itsame@mario.com <itsame@mario.com>
2023-12-23 17:12:13 +0500

    test 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>HEYAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa</p>
+      <p>HIIIIIIIIII</p>
+      <div className="img-wrap">
+        <div className="img-out">
+          <Image src="/one.jpeg" alt="ONEE" className="img-in" fill={true} />
+        </div>
+        <div className="img-out">
+          <Image src="/two.jpg" alt="ONEE" className="img-in" fill={true} />
+        </div>
+        <div className="img-out">
+          <Image src="/three.jpeg" alt="ONEE" className="img-in" fill={true} />
+        </div>
+        <div className="img-out">
+          <Image src="/four.jpg" alt="ONEE" className="img-in" fill={true} />
+        </div>
+        <div className="img-out">
+          <Image src="/five.jpg" alt="ONEE" className="img-in" fill={true} />
+        </div>
+      </div>
     </main>
-  )
+  );
 }
